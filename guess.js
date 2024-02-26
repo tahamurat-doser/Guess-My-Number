93d13e132ad7fa9d14830c11e7c6616b5c52c1f2
taha_ murat <ttahamuratdoser@gmail.com>
2024-02-26 14:46:13 +0300

    localstorage

--- guess.js
+++ guess.js
@@ -15,6 +15,7 @@ girlenSyi.onclick = () => {
 document.querySelector(".check").addEventListener("click", () => {
   const tahmin = document.querySelector(".guess").value;
   /*  console.log(tahmin); */
+  document.querySelector(".guess").value = "" 
   if (!tahmin) {
     //! tahmin girilmediği durumlarda if in çalışmasını istiyorum bu yüzden de tahmin girilmediğinde değeri false olur ama ben if bloğunun çalışmasını istediğimden ! koyarak false u true yaparım.
     mesaj.textContent = "Lütfen bir sayı giriniz...🙂";
@@ -22,6 +23,14 @@ document.querySelector(".check").addEventListener("click", () => {
     mesaj.textContent = "Tebrikler bildiniz...👏🏻";
     document.querySelector("body").style.backgroundColor = "green";
     document.querySelector(".number").textContent = rastgeleSayi;
+    if(skor > enYuksekSkor) {
+      enYuksekSkor = skor
+      document.querySelector(".top-score").textContent = skor
+    }
+
+
+
+
   } else {
     if (skor > 1) {
       skor--;
@@ -31,7 +40,32 @@ document.querySelector(".check").addEventListener("click", () => {
         ? (mesaj.textContent = "Arttır 👆🏻")
         : (mesaj.textContent = "Azalt 👇🏻");
     } else {
-        
+        mesaj.textContent = "Oyunu kaybettiniz🥺"
+        document.querySelector(".score").textContent = 0;
+        document.querySelector("body").style.backgroundColor ="red"
+        document.querySelector(".number").textContent = rastgeleSayi;
+
+
+
     }
   }
 });
+
+document.querySelector(".again").onclick = () => {
+  document.querySelector("body").style.backgroundColor ="#2d3436"
+  rastgeleSayi = Math.ceil(Math.random()*20)
+  skor = 10
+  document.querySelector(".score").textContent = skor
+  document.querySelector(".number").textContent = "?"
+  document.querySelector(".guess").value = "" 
+  mesaj.textContent = "Oyun yeniden başlıyor..."
+}
+
+document.addEventListener("keydown", function(e){
+  if (e.key === "Enter") {
+    document.querySelector(".check").click()
+    
+  } 
+})
+
+localStorage.setItem()
